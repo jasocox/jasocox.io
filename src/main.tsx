7a9c6ef0d5988d37e14ce212e298cd0b8464727c
Jason Cox <jasonwilburcox@gmail.com>
2026-01-26 15:08:41 -0500

    React: Setup React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    Component: App,
+  },
+]);
+
+createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
